1100c57184339a81b2cf6a7f157efbae5cf759fd
SeregaYakovlev <sergo.yakovlev@yandex.ru>
2025-05-30 00:24:16 +0300

    fix

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
 public class Main {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_PURPLE = "\u001B[95m";
     public static final String ANSI_BLUE = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
